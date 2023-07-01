9649a1c8760483e65e2dce826541ed604f3391fd
Bruno <brunooliveirasilva77@gmail.com>
2023-06-30 22:25:57 -0300

    Finalizando atividades requisitados

--- app/src/main/java/com/example/likeimage/LikesViewModel.java
+++ app/src/main/java/com/example/likeimage/LikesViewModel.java
@@ -1,18 +1,26 @@
 package com.example.likeimage;
 
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
 
-public class LikesViewModel {
+public class LikesViewModel extends ViewModel {
+
     private MutableLiveData<String> nome;
     private MutableLiveData<String> sobrenome;
     private MutableLiveData<Integer> like;
-    private MutableLiveData<Integer> image;
 
-    public LikesViewModel(MutableLiveData<String> nome, MutableLiveData<String> sobrenome, MutableLiveData<Integer> like, MutableLiveData<Integer> image) {
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.like = like;
-        this.image = image;
+    private MutableLiveData<Integer> imagem;
+
+    public LikesViewModel() {
+        imagem = new MutableLiveData<>(R.drawable.like);
+        nome = new MutableLiveData<>("Bruno");
+        sobrenome = new MutableLiveData<>("Silva");
+        like = new MutableLiveData<>(0);
+    }
+
+    public MutableLiveData<Integer> getLike() {
+        return like;
     }
 
     public MutableLiveData<String> getNome() {
@@ -23,15 +31,19 @@ public class LikesViewModel {
         return sobrenome;
     }
 
-    public MutableLiveData<Integer> getLike() {
-        return like;
+    public LiveData<Integer> getImagem() {
+        return imagem;
     }
 
-    public MutableLiveData<Integer> getImage() {
-        return image;
-    }
-
-    public void darLike(MutableLiveData<Integer> like) {
-        this.like = like;
+    public void darLike() {
+        like.setValue((like.getValue()+1));
+        if(like.getValue() <= 3){
+            imagem.setValue(R.drawable.like);
+        } else if (like.getValue() <= 9) {
+            imagem.setValue(R.drawable.coracao);
+        } else {
+            imagem.setValue(R.drawable.fogo);
+        }
     }
 }
+
--- app/src/main/java/com/example/likeimage/MainActivity.java
+++ app/src/main/java/com/example/likeimage/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.likeimage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+
+import com.example.likeimage.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setLifecycleOwner(this);
+
+        LikesViewModel vm = new ViewModelProvider(this).get(LikesViewModel.class);
+
+        binding.setVm(vm);
+
     }
 }
